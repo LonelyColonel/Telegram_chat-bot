287747e243daaf8b61354139899c9d37df609921
PC_my <user@gmail.com>
2022-03-15 20:52:07 +0300

    0.2

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import logging
 import sys
 import os
+import telegram
+from telegram import Bot
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler, CallbackQueryHandler, ChosenInlineResultHandler
-from telegram import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup, Update
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
+# logger.setLevel(logging.DEBUG)
 
 
 class MainBot:
@@ -12,10 +20,12 @@ class MainBot:
         self.token = token
         self.updater = Updater(self.token, use_context=True)
         self.dp = self.updater.dispatcher
+
+        self.bot = telegram.Bot(self.token)
         self.reply_keyboard = [['Разделы'], ['Помощь', 'О боте']]
         self.markup = ReplyKeyboardMarkup(self.reply_keyboard, one_time_keyboard=False)
 
-    def echo(self, update, context):
+    def echo(self, update: Update, context: CallbackContext):
         # У объекта класса Updater есть поле message,
         # являющееся объектом сообщения.
         # У message есть поле text, содержащее текст полученного сообщения,
@@ -30,6 +40,7 @@ class MainBot:
         )
 
     def help(self, update, context):
+        self.bot.sendMessage(chat_id=update.message.chat_id, text='GG')
         update.message.reply_text(
             'Это раздел помощь')
 
@@ -41,6 +52,7 @@ class MainBot:
 
     def sections(self, update, context):
         # список кнопок
+        self.chat_id = update.message.chat_id
         button_list = [
             [InlineKeyboardButton("Физика(ЕГЭ)", callback_data='ФИЗИКА(ЕГЭ)')],
             [InlineKeyboardButton("Математика(олимпиады)", callback_data='МАТЕМАТИКА(олимпиады)')],
@@ -60,46 +72,46 @@ class MainBot:
         # в чате заменятся на этот ответ.
         query.edit_message_text(text=f'Выбран раздел: {variant}')
         if variant == 'ФИЗИКА(ЕГЭ)':
-            # print(variant)
-            # print(type(update))
-            work_class = Physics(self.token)
+            print(update)
+            work_class = Physics(self.token, self.bot, self.chat_id, update)
             work_class.main_physics()
-        # self.dp.add_handler(ChosenInlineResultHandler(variant), group=Physics.main_physics)
+
+    def main(self):
+        self.dp.add_handler(MessageHandler(Filters.text('123'), self.echo))
+        self.dp.add_handler(MessageHandler(Filters.text('Помощь'), self.help))
+        # Регистрируем обработчик в диспетчере.
+        self.dp.add_handler(CommandHandler('start', self.start))
+        self.dp.add_handler(MessageHandler(Filters.text('О боте'), self.about_bot))
+        self.dp.add_handler(MessageHandler(Filters.text('Разделы'), self.sections))
+        self.dp.add_handler(CallbackQueryHandler(self.choice))
+        # Запускаем цикл приема и обработки сообщений.
+        self.updater.start_polling()
+
+        # Ждём завершения приложения.
+        self.updater.idle()
 
 
-class Physics:
-    def __init__(self, token):
-        self.token = token
-        self.updater = Updater(self.token, use_context=True)
-        self.dp = self.updater.dispatcher
-        # super().__init__(self)
+class Physics(MainBot):
 
-    def oo(self, update, _):
-        update.message.reply_text(text='физика_тестовый_принт')
-    # def main_physics(self, update, _):
-    #     update.message.reply_text(text='физика_тестовый_принт')
+    def __init__(self, token, bot, chat_id, update):
+        super().__init__(token)
+        self.bot = bot
+        self.chat_id = chat_id
+        self.update = update
 
     def main_physics(self):
-        self.dp.add_handler(MessageHandler(Filters.all(), self.oo))
-
-
-def main():
-    global reply_keyboard
-    main_bot = MainBot('5136885850:AAFx-0jdcaXWeiY-0VObbXdFaECnQiku2nc')
-    main_bot.dp.add_handler(MessageHandler(Filters.text('123'), main_bot.echo))
-    main_bot.dp.add_handler(MessageHandler(Filters.text('Помощь'), main_bot.help))
-    # Регистрируем обработчик в диспетчере.
-    main_bot.dp.add_handler(CommandHandler('start', main_bot.start))
-    main_bot.dp.add_handler(MessageHandler(Filters.text('О боте'), main_bot.about_bot))
-    main_bot.dp.add_handler(MessageHandler(Filters.text('Разделы'), main_bot.sections))
-    main_bot.dp.add_handler(CallbackQueryHandler(main_bot.choice))
-
-    # Запускаем цикл приема и обработки сообщений.
-    main_bot.updater.start_polling()
-
-    # Ждём завершения приложения.
-    main_bot.updater.idle()
+        print('main_physics')
+        # button_list = [
+        #     [InlineKeyboardButton("Термодинамика", callback_data='ФИЗИКА(ЕГЭ)')],
+        #     [InlineKeyboardButton("Математика(олимпиады)", callback_data='МАТЕМАТИКА(олимпиады)')],
+        # ]
+        # reply_markup = InlineKeyboardMarkup(button_list)
+        # # отправка клавиатуры в чат
+        # self.bot
+        # self.bot.sendMessage.reply_text(text="Меню из двух столбцов", reply_markup=reply_markup)
+        self.bot.sendMessage(chat_id=self.chat_id, text='GG')
 
 
 if __name__ == '__main__':
-    main()
+    main_bot = MainBot('5136885850:AAFx-0jdcaXWeiY-0VObbXdFaECnQiku2nc')
+    main_bot.main()
